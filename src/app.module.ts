9da58468b69d869b27ea569038351a4cca058480
jstevick+1-at-666254836729 <joelstevick@outlook.com>
2020-12-15 18:20:42 -0500

    configured as an apollo fed-gw

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLGatewayModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-
+  imports: [
+    GraphQLGatewayModule.forRoot({
+      server: {
+        // ... Apollo server options
+        cors: true,
+      },
+      gateway: {
+        serviceList: [],
+      },
+    }),
+  ],
 })
 export class AppModule {}
